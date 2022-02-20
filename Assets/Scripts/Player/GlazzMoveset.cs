f695b93a4db2bdcc00fc12e7b98bec4fc938ec7d
Davi DEV <40278000+davimatiassg@users.noreply.github.com>
2022-02-19 21:08:59 -0300

    2022 - 02 - 19 - Testing Tileset Updates

--- Assets/Scripts/Player/GlazzMoveset.cs
+++ Assets/Scripts/Player/GlazzMoveset.cs
@@ -6,55 +6,60 @@ public class GlazzMoveset : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
     [SerializeField] private int life = 50;
-
-
-
-
-    
-
     [SerializeField] private int direction = 0;
     //0 = Down, 1 = Up, 2 = Right, 3 = Left.
     public int state = 0; 
     //0 = Idle, 1 = Moving, 2 = Melee atk, 3 = Magic atk, 4 = Dashing, 5 = stuned, 6 = cutscene.
 
 
+    //movimentation Variables
+
+    public Vector3 deltaMove;
 
 
     private Transform trs;
     [SerializeField] private Transform glz;
     private Animator anim;
+    private BoxCollider2D coll;
     private Rigidbody2D rigb;
 
     void Start()
     {	
     	trs = this.gameObject.GetComponent<Transform>();
         anim = glz.gameObject.GetComponent<Animator>();
-        rigb = this.gameObject.GetComponent<Rigidbody2D>();
+        coll = this.gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        rigb.velocity = InputMoviment();
+        deltaMove = InputMoviment();
+        deltaMove = ColisionDetect();
+        trs.Translate(deltaMove * Time.fixedDeltaTime);
         PlayAnim(direction, state);
         FloatingEffect();
     }
+
+
     public Vector2 InputMoviment()
     {
     	float y = Input.GetAxisRaw("Vertical");
     	float x = Input.GetAxisRaw("Horizontal");
+    	if(y*y + x*x != 0)
+    	{
+    		direction = Mathf.RoundToInt((1+y)/2  - Mathf.Abs(x)*(1+y)/2  + Mathf.Abs(x)*(5 - x)/2);
+    	}
+    	
+/*
     	if(y != 0f && x == 0f)
 		{
 			if(y > 0)
 			{
-				direction = 1;
-                
-				//anim.Play("up-idle");			
+				direction = 1;		
 			}
 			else
 			{
 				direction = 0;
-				//anim.Play("down-idle");
                 
 			}
 		}
@@ -62,18 +67,14 @@ public class GlazzMoveset : MonoBehaviour
     	{
 			if(x > 0)
 			{
-				direction = 2;
-				//anim.Play("right-idle");	
-                		
+				direction = 2;               		
 			}
 			else
 			{
-				direction = 3;
-				//anim.Play("left-idle");
-                
+				direction = 3;                
 		    }
     	}
-
+*/
         Vector2 v = new Vector2(x, y).normalized*speed;
         if(v != Vector2.zero)
         {
@@ -84,8 +85,23 @@ public class GlazzMoveset : MonoBehaviour
             state = 0;
         }
     	return v;
+    }
 
-
+    private Vector2 ColisionDetect()
+    {
+    	Vector2 V = Vector2.zero;
+    	RaycastHit2D hit = Physics2D.BoxCast(trs.localPosition, coll.size, 0, deltaMove.y*Vector2.up, Mathf.Abs(deltaMove.y*Time.fixedDeltaTime), LayerMask.GetMask("Blocking", "Water"));
+    	if(hit.collider == null)
+    	{
+    		V += Vector2.up*deltaMove;
+    	}
+    	hit = Physics2D.BoxCast(trs.localPosition, coll.size, 0, deltaMove.x*Vector2.right, Mathf.Abs(deltaMove.x*Time.fixedDeltaTime), LayerMask.GetMask("Blocking", "Water"));
+    	if(hit.collider == null)
+    	{
+    		V += Vector2.right*deltaMove;
+    	}
+    	Debug.Log(V);
+    	return V;
     }
 
     public void PlayAnim(int dir, int st)
@@ -95,6 +111,8 @@ public class GlazzMoveset : MonoBehaviour
     }
     private void FloatingEffect()
     {
-    	glz.position += Mathf.Cos(Time.time*2)*Vector3.up/500;
+    	glz.localPosition = (Mathf.Sin(Time.time*2)/5)*Vector3.up;
+    
     }
+
 }
---
+++ Assets/TilePalletes/SandBasing.tsx
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="SandBasing" tilewidth="32" tileheight="32" tilecount="288" columns="18">
+ <image source="../Textures/Scenary/Pink Dunes.png" width="576" height="512"/>
+ <terraintypes>
+  <terrain name="Blocks" tile="227"/>
+ </terraintypes>
+ <tile id="149" terrain=",,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="20"/>
+   <object id="2" x="0" y="0" width="20" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="150" terrain=",,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="151" terrain=",,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12" y="0" width="20" height="32"/>
+   <object id="2" x="0" y="0" width="32" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="163">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="164">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="166" terrain=",,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="20">
+    <polygon points="0,0 -12.001,11.9609 -32,11.9565 0,-20.0435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="167" terrain=",0,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="0">
+    <polygon points="3.95078,-0.117448 -16.0547,19.8906 -16.0625,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="168" terrain="0,0,0,0"/>
+ <tile id="169" terrain="0,,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12" y="0">
+    <polygon points="0,-0.0546875 20,19.9453 20,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="170" terrain=",,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="20">
+    <polygon points="0,0 12.0044,12.0044 32,12 0,-20"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="184" terrain=",0,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="20" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="185" terrain="0,0,0,0"/>
+ <tile id="186" terrain="0,0,0,0"/>
+ <tile id="187" terrain="0,0,0,0"/>
+ <tile id="188" terrain="0,,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12" y="0" width="20" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="198">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 7.01425,7.93465 16.0433,7.92101 16.0085,-0.0625"/>
+   </object>
+   <object id="2" x="32" y="0">
+    <polygon points="-0.0078125,0.0625 -0.00277778,24.0861 -15.9688,8.17896 -16,0.087345"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="201">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 -7.80722,8.87273 -15.8249,8.86426 -15.7901,-0.116162"/>
+   </object>
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 0.272727,23.7898 16.0114,9.01991 16.1058,-0.0202311"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="202" terrain=",0,,">
+  <objectgroup draworder="index" id="4">
+   <object id="3" x="0.181818" y="0.181818">
+    <polygon points="-0.181818,-0.181818 -0.181818,31.8182 31.8182,31.8182 31.8182,13 19.7273,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="203" terrain="0,0,,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="16">
+    <polygon points="-0.363636,-3.45028 18.6388,16.5522 -0.35973,16.5543"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="204" terrain="0,0,0,0"/>
+ <tile id="205" terrain="0,0,0,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="16">
+    <polygon points="0.015625,-2 -17.9631,16.0099 0.010101,16.002"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="206" terrain="0,,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13" y="0">
+    <polygon points="0,0 -12.9091,13.5455 -13,32 19,32 19,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="217">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="-8">
+    <polygon points="0,0 32.1818,31.9091 32.3636,0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="218">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="-8">
+    <polygon points="0,0 -32,32 -32,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="221" terrain=",0,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.1875" width="17.5" height="32.125"/>
+   <object id="2" x="0" y="14.5625" width="32" height="17.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="222" terrain="0,0,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="20" width="32" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="223" terrain="0,,,">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.6875" y="0.125" width="17.25" height="32.25"/>
+   <object id="2" x="0.0625" y="13.5" width="31.875" height="18.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="256">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 32.2727,31.9091 32.2727,-0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="257">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="259">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="260">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0">
+    <polygon points="0,0 -32,32 -32,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="271">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="23">
+    <polygon points="-0.0888889,0.777778 -14.1016,9.00994 -32.0909,9 -32.0909,-23 -0.0909091,-23"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="272">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="273">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.1818" y="32">
+    <polygon points="0,0 -14.0625,-8.25 -14.125,-32.125 17.875,-32.125 17.875,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="275">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="276">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="12" width="32" height="20"/>
+  </objectgroup>
+ </tile>
+ <tile id="277">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
